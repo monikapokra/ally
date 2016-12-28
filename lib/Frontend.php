2e9ae6cb1fc69be6160510cfe18a5958ab58ae3e
Monika Gupta <Monika Gupta>
2016-12-28 17:50:50 +0530

    commit message

--- lib/Frontend.php
+++ lib/Frontend.php
@@ -11,7 +11,7 @@ class Frontend extends ApiFrontend {
                 'php'=>['shared']
             ))
             ->setBasePath($this->pathfinder->base_location->getPath());
-        
+        asdasdds
         // Might come handy when multi-timezone base networks integrates
         $this->today = date('Y-m-d',strtotime($this->recall('current_date',date('Y-m-d'))));
         $this->now = date('Y-m-d H:i:s',strtotime($this->recall('current_date',date('Y-m-d H:i:s'))));
