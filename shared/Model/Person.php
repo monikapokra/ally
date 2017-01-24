02062acb688f12d880361979a25efe57e61ff515
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-01-24 18:57:40 +0530

    invalidate email if @(\d+). is format

--- shared/Model/Person.php
+++ shared/Model/Person.php
@@ -63,6 +63,10 @@ class Model_Person extends Model_MyTable{
 		if(strlen(trim($this['address'])) >0 AND strlen(trim($this['address']))<=20)
 			throw $this->exception('Address must be of length 20 or more','ValidityCheck')->setField('address');
 
+		if(!$this->validEmail($this['email']))
+			throw $this->exception('Email is not valid','ValidityCheck')->setField('email');
+
+
 		if($this['type']=='Student'){
 			$old_student = $this->add('Model_Person')
 								->addCondition('enrollment_no',$this['enrollment_no'])
@@ -74,5 +78,27 @@ class Model_Person extends Model_MyTable{
 			
 		}
 		
+	}
+
+	function validEmail($email){
+
+		$email_array = explode("@", $email);
+		$local_array=explode(".", $email_array[1]);
+		if(is_numeric($local_array[0])) return false;
+
+	    if(is_array($email) || is_numeric($email) || is_bool($email) || is_float($email) || is_file($email) || is_dir($email) || is_int($email))
+	        return false;
+	    else
+	    {
+	        $email=trim(strtolower($email));
+	        if(filter_var($email, FILTER_VALIDATE_EMAIL)===true) return $email;
+	        else
+	        {
+	            $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+	            return (preg_match($pattern, $email) === 1) ? $email : false;
+	        }
+	    }
+
+	    return true;
 	}
 }
